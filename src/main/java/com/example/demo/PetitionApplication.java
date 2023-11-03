e3cf87d8b018433b0eeef35dce1f696b67eb0fc9
Michael Walsh <walshmichael310@gmail.com>
2023-11-03 05:48:21 +0000

    Adding initial structure for main page This takes care of adding a new path and the initial structure for the homepage which will provide a view of all petitions.

---
+++ src/main/java/com/example/demo/PetitionApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.*;
+
+@SpringBootApplication
+@RestController
+public class PetitionApplication {
+
+	@RequestMapping("/")
+	public ModelAndView viewPetitions() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("petitions.html");
+		return modelAndView;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(PetitionApplication.class);
+
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "9090"));
+		app.run(args);
+	}
+
+}
