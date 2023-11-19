5cfe57f17c802d6f0a5f4ac651e0a7f4b2fe696f
Michael Walsh <walshmichael310@gmail.com>
2023-11-19 18:00:59 +0000

    Adding test for PetitionController This includes the addition of one test for the petition controller

--- src/test/java/com/michaelspetitions/controller/PetitionControllerTests.java
+++ src/test/java/com/michaelspetitions/controller/PetitionControllerTests.java
@@ -1,13 +1,20 @@
 package com.michaelspetitions.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PetitionControllerTests {
 
+	@Autowired
+	private PetitionController petitionController;
+
 	@Test
 	void contextLoads() {
+		assertThat(petitionController).isNotNull();
 	}
 
 }
